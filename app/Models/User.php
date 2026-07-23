8276250c0d5dcebd5b620ab9c835d7b090bf64a5
Phatyuth <phatyuthyou9@gmail.com>
2026-07-23 09:56:01 +0700

    Fix image

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
 
 class User extends Authenticatable
 {
@@ -164,7 +165,7 @@ class User extends Authenticatable
         }
 
         if (Str::startsWith($path, 'profile_photos/') || Str::startsWith($path, 'avatars/')) {
-            return asset('storage/' . $path);
+            return Storage::disk('public')->url($path);
         }
 
         return asset($path);
